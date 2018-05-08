900d1165e217dd6e5876b7358c5043f272a2f172
M0ca <blueskytime@naver.com>
2018-05-08 13:48:07 +0900

    노트 여러개 떨구기

--- Source/Note.cpp
+++ Source/Note.cpp
@@ -2,10 +2,12 @@
 #include "Note.h"
 Note::Note() : bpm(0)
 {
-	setFramesPerSecond(120);
+	setFramesPerSecond(60); // bpm조절
 }
+
 Note::~Note()
 { 
+
 } 
 
 void Note::update()
@@ -16,13 +18,14 @@ void Note::update()
 
 void Note::paint (Graphics& g)
 {
-	std::cout << getMillisecondsSinceLastUpdate() << std::endl;
 	// 무지개 노트
 	g.fillAll(Colour(13,13,13));
 	
 	g.setColour(Colours::red);
 //	g.setColour (Colour((juce::uint32) Random::getSystemRandom().nextInt()).withAlpha(0.5f).withBrightness(0.7f));
-	g.fillRect(block);
+	for(auto& i : note_list)
+		g.fillRect(i);
+	//g.fillRect(block);
 }
 
 void Note::resized()
@@ -33,9 +36,21 @@ void Note::resized()
 void Note::SetNotePos(const Rectangle<float>& rect)
 {
 	block = rect;
+	AddNote(rect);
 }
 
 void Note::DownNote(){
+	for(auto& i : note_list){
+		if(i.getY() <= getHeight()/12*10.5+getHeight()/30){
+				i.setY(i.getY()+20);
+		} else{
+			//note_list.pop_front();
+				i.setX(getWidth()/12 * Random::getSystemRandom().nextInt(Range<int>(4,8)));
+			i.setY(-10);
+			//bpm++;
+		}
+	}
+	/*
 	if(block.getY() <= getHeight()/12*10.5+getHeight()/30){
 		block.setY(block.getY()+20);
 	}
@@ -45,9 +60,14 @@ void Note::DownNote(){
 		block.setY(-10);
 		bpm++;
 	}
+	*/
+	if(Random::getSystemRandom().nextInt(Range<int>(1,5)) == 2)
+	{
+		AddNote(block);
+	}
 }
 
 void Note::AddNote(const Rectangle<float>& rect)
 {
-
+	note_list.push_back(rect);
 }
--- Source/Note.h
+++ Source/Note.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "../JuceLibraryCode/JuceHeader.h"
-
+#include <list>
 /* 노트 떨어지는 그래픽 */
+
+/* 생성되는 노트를 큐에 넣고 라인이 지나면 앞에서부터 뺸다.
+ */
 
 class Note    : public AnimatedAppComponent 
 {
@@ -17,6 +20,7 @@ public:
 	void SetNotePos(const Rectangle<float>& rect);
 	//getMillisecondsSinceLastUpdate() update호출한수
 private:
+	std::list<Rectangle<float>> note_list;
 	Rectangle<float> block;
 	unsigned int bpm;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Note)
